355eb2a256bb6dc78d3c73cf279fc886929d3c45
Matthew Quesada <quesadamatthew@yahoo.com>
2018-08-24 09:29:42 -0700

    Refactored AddIngredientForm component from stateless functional component to class-based component

--- src/components/leftCard/addIngredientForm/AddIngredientForm.js
+++ src/components/leftCard/addIngredientForm/AddIngredientForm.js
@@ -1,18 +1,40 @@
 import React from 'react';
 
-const AddIngredientForm = () => {
-  const handleIngredientAdd = () => {
-    alert("Ingredient Added");
+class AddIngredientForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  return (
-    <div class="AddIngredientForm-wrapper">
-      <form className="form-inline my-2 my-lg-0" onSubmit={handleIngredientAdd}>
-        <input className="form-control mr-sm-2" type="text" placeholder="Ingredient" />
-        <button className="btn btn-primary" type="submit">Add</button>
-       </form>
-    </div>
-  );
+  // update state on key-press
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.value);
+    this.setState({ value: '' });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div class="AddIngredientForm-wrapper">
+        <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
+          <input  className="form-control mr-sm-2" 
+                  type="text" 
+                  placeholder="Ingredient" 
+                  value={this.state.value}
+                  onChange={this.handleChange} 
+          />
+          <button className="btn btn-primary" type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddIngredientForm;
